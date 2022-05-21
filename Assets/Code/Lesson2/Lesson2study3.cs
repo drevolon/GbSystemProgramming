116bde7614a96082136abdba6a57e4b1b519b14a
drevolon <zelenand@outlook.com>
2022-05-22 03:00:04 +0500

    Lesson2 study 3

--- Assets/Code/Lesson2/Lesson2study3.cs
+++ Assets/Code/Lesson2/Lesson2study3.cs
@@ -7,34 +7,69 @@ using UnityEngine.Jobs;
 
 public class Lesson2study3 : MonoBehaviour
 {
-    private NativeArray<Vector3> _transform;
+    [SerializeField] GameObject _prefab;
+    [SerializeField] int _count=10;
+    [SerializeField] float _speed = 100;
+
+    TransformAccessArray _transformAccess;
+    Transform[] _objectData;
+    MyJobs myJobs;
+    
+
 
     void Start()
     {
-        _transform = new NativeArray<Vector3>(100, Allocator.Persistent);
+        _objectData= SpawnData(_prefab, _count);
 
-        for (int i = 0; i < 100; i++)
-        {
-            _transform[i] = new Vector3(1, 1, 1);
-        }
+        _transformAccess = new TransformAccessArray(_objectData);
 
-        MyJobs myJobs = new MyJobs() { transform = _transform };
-        JobHandle jobHandle = myJobs.Schedule(100,5, default);
+        myJobs = new MyJobs(_speed,Time.deltaTime);
+
+        JobHandle jobHandle = myJobs.Schedule(_transformAccess);
         jobHandle.Complete();
+    }
+
+    private void Update()
+    {
+        myJobs._deltaTime = Time.deltaTime;
+        var handle=myJobs.Schedule(_transformAccess);
+        handle.Complete();
     }
 
     public struct MyJobs : IJobParallelForTransform
     {
-        public NativeArray<Vector3> transform;
+        private float _speed;
+        public float _deltaTime;
+        public MyJobs(float speed, float deltaTime)
+        {
+            _speed = speed;
+            _deltaTime = deltaTime;
+        }
 
         public void Execute(int index, TransformAccess transform)
         {
             Debug.Log($"MyJobs transform: {transform}");
+            
+            transform.rotation = Quaternion.Euler(_speed * _deltaTime, transform.position.y, transform.position.z);
         }
+    }
+
+    private Transform[] SpawnData(GameObject prefab, int count)
+    {
+        Transform[] spawn=new Transform[count];
+        for (int i = 0; i < count; i++)
+        {
+            Transform spawnObject= Instantiate(prefab).transform;
+            spawnObject.position = new Vector3 (Random.Range(1,5), Random.Range(1, 5), Random.Range(1, 5));
+            spawn[i] = spawnObject;
+        }
+        return spawn;
     }
 
     private void OnDestroy()
     {
-        if (_transform.IsCreated) _transform.Dispose();
+        if (_transformAccess.isCreated) _transformAccess.Dispose();
     }
+
+
 }
